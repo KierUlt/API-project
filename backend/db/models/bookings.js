89edaeca93da216cc91d8e5ab572104d8adcc245
Erik <spike158899@gmail.com>
2023-02-22 22:40:13 -0800

    db info inserted, incomplete

---
+++ backend/db/models/bookings.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Bookings extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Bookings.belongsTo(
+        models.Spots,
+        { foreignKey: 'spotId'}
+      );
+      Bookings.belongsTo(
+        models.User,
+        { foreignKey: 'userId'}
+      )
+    }
+  }
+  Bookings.init({
+    spotId: {
+      type: DataTypes.INTEGER
+    },
+    userId: {
+      type: DataTypes.INTEGER
+    },
+    startDate: {
+      type: DataTypes.DATE
+    },
+    endDate: {
+      type: DataTypes.DATE
+    }
+  }, {
+    sequelize,
+    modelName: 'Bookings',
+  });
+  return Bookings;
+};
