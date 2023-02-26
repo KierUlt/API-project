552bb0e2af02d9e7c326a22c48960fb6cb867754
Erik <spike158899@gmail.com>
2023-02-25 23:24:59 -0800

    get current user's reviews complete

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,9 +1,53 @@
 const express = require('express');
-const { Reviews } = require('../../db/models');
+const { requireAuth } = require("../../utils/auth");
+const { Reviews, Spots, User, SpotImages, ReviewImages, sequelize } = require('../../db/models');
 const router = express.Router();
 
-router.get('/current', async (req, res) => {
-
+router.get('/current', requireAuth, async (req, res) => {
+    const { user } = req;
+    const userReviews = await Reviews.findAll({
+        where: {
+            userId: user.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'firstName', 'lastName']
+            },
+            {
+                model: Spots,
+                attributes: {
+                    exclude: ['description', 'createdAt', 'updatedAt']
+                },
+                include: [
+                    {
+                        model: SpotImages,
+                        attributes: ['preview', 'url']
+                    }
+                ]
+            },
+            {
+                model: ReviewImages,
+                attributes: ['id', 'url']
+            }
+        ]
+    })
+    if(userReviews.length <= 0) return res.json({message: "No reviews found"});
+    let foundReviews = [];
+    userReviews.forEach(review => {
+        review = review.toJSON();
+        if(review.Spot.SpotImages.length > 0){
+            for(let prev in review.Spot.SpotImages){
+                console.log(review.Spot.SpotImages[prev])
+                if(review.Spot.SpotImages[prev].preview) review.Spot.previewImage = review.Spot.SpotImages[prev].url
+            }
+        } else review.Spot.previewImage = "No preview image found";
+        if(review.ReviewImages.length <= 0) review.ReviewImages = "No review image found";
+        delete review.Spot.SpotImages;
+        foundReviews.push(review);
+    });
+    let pushedReviews = { Reviews: foundReviews };
+    res.json(pushedReviews);
 });
 
 router.post('/:reviewId/images', async (req, res) => {
