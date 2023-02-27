72b5518088a7bf8131999ed5845a1dd86c549890
Erik <spike158899@gmail.com>
2023-02-27 09:41:13 -0800

    fixed migrations typo

--- backend/db/models/spots.js
+++ backend/db/models/spots.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
       );
       Spots.hasMany(
         models.SpotImages,
-        { foreignkey: 'spotId', onDelete: 'CASCADE'}
+        { foreignKey: 'spotId', onDelete: 'CASCADE'}
       );
     }
   }
