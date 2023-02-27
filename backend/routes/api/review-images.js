a6145033b3f5139691c5851271c660f147b0cbb8
Erik <spike158899@gmail.com>
2023-02-26 22:54:53 -0800

    delete review image complete

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,9 +1,31 @@
 const express = require('express');
-const { ReviewImages } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+const { ReviewImages, sequelize } = require('../../db/models');
 const router = express.Router();
 
-router.delete('/:imageId', async (req, res) => {
-
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    let { imageId } = req.params;
+    let user = req.user;
+    let img = await ReviewImages.findByPk(imageId);
+    if(!img){
+        let err = {
+            status: 404,
+            statusCode: 404,
+            message: "Review Image couldn't be found"
+        }
+        return next(err);
+    }
+    let review = await img.getReview();
+    if(user.id !== review.userId){
+        let err = {
+            status: 403,
+            statusCode: 403,
+            message: "You do not have proper authorization to delete the image"
+        }
+        return next(err);
+    }
+    await img.destroy();
+    res.json({message: "Successfully deleted", statusCode: 200})
 });
 
 
