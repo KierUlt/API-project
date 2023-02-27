d3280251f8bf5ea4bb04b8f44d080613bb72b10b
Erik <spike158899@gmail.com>
2023-02-26 22:40:41 -0800

    completed delete Spot Image

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -1,9 +1,38 @@
 const express = require('express');
-const { SpotImages } = require('../../db/models');
+const { requireAuth } = require("../../utils/auth")
+const { SpotImages, Spots, sequelize } = require('../../db/models');
 const router = express.Router();
 
-router.delete('/:imageId', async (req, res) => {
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    let { user } = req;
+    let imgId = req.params.imageId;
+    let imgToDelete = await SpotImages.findByPk(imgId, {
+        include: [
+            {
+                model: Spots
+            }
+        ]
+    });
 
+    if(!imgToDelete){
+        let err = {
+            status: 404,
+            statusCode: 404,
+            message: "Spot Image couldn't be found"
+        }
+        return next(err);
+    }
+    if(imgToDelete.Spot.ownerId !== user.id){
+        let err = {
+            status: 403,
+            statusCode: 403,
+            message: "You do not have proper authorization to delete the image"
+        }
+        return next(err);
+    }
+    await imgToDelete.destroy();
+    res.status(200);
+    res.json({message: "Successfully deleted", statusCode: 200})
 });
 
 module.exports = router;
